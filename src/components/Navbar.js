415279f1e7a883fb28f2e75ac18ac6eddfd5a87a
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2025-01-24 13:49:58 +0530

    Accessing state in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+  const amount=useSelector(state => state.amount)
   return (
   <>
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">PNB</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -21,7 +23,7 @@ const Navbar = () => {
       </ul>
       
       <div>
-        <button disabled={true} className="btn-primary">Your Balance: 0</button>
+        <button disabled={true} className="btn-primary">Your Balance: {amount}</button>
       </div>
     </div>
   </div>
