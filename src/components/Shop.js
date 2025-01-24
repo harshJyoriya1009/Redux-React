20a579de3e595a69e44459a576131ddb07f97c3f
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2025-01-24 14:10:32 +0530

    Updating shop in components

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+// import { bindActionCreators } from 'redux';
+import { actionCreators } from '../state';
+import { bindActionCreators } from 'redux';
 
 const Shop = () => {
+    const dispatch=useDispatch();
+    const {withdrawMoney, depositMoney}=bindActionCreators(actionCreators, dispatch)
   return (
     <>
     <h3>Deposit / Withdraw Money</h3>
-    <button className="btn-primary mx-2">-</button>
+    {/* <button className="btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
     Bank Balance
-    <button className="btn-primary mx-2">+</button>
+    <button className="btn-primary mx-2"onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button> */}
+
+
+    <button className="btn-primary mx-2" onClick={()=>{withdrawMoney(100)}}>-</button>
+    Bank Balance
+    <button className="btn-primary mx-2"onClick={()=>{depositMoney(100)}}>+</button>
     </>
   )
 }
